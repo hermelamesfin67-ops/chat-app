f2683f6c142fe7154a9908e5b1b8e3fb1667755c
hermelamesfin67-ops <hermelamesfin67@gmail.com>
2026-08-26 14:28:24 +0300

    add get asgi application in asgi.py

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -11,14 +11,16 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.middleware import JWTAuthMiddleware
 import chat.routing
 
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
 
     "websocket": JWTAuthMiddleware(
         URLRouter(
